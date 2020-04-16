2ba36cce65d1d0be860db54572d553efaa2f4905
Paulo Eduardo Oliveira da Silva <pesilva23@outlook.com>
2020-04-16 13:19:36 -0300

    Implementação da lógida da calculadora

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,65 +1,110 @@
-import React from 'react';
-import {StatusBar, SafeAreaView, Text} from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {
-  Container,
-  ScrollView,
-  Footer,
-  Engine,
-  Body,
-  SectionContainer,
-  SectionTitle,
-  SectionDescription,
-  Highlight,
-} from './styles';
+import React, {useState, useEffect} from 'react';
+import {Container, Buttons} from './styles';
+import Button from '../components/Button/index';
+import Display from '../components/Display/index';
 
 export default function Home() {
+  // Declara o Hook de State das váriáveis que serão utilizadas
+  const [displayValue, setDisplayValue] = useState('0');
+  const [clearDisplay, setClearDisplay] = useState(false);
+  const [nextOperation, setNextOperation] = useState(null);
+  const [values, setValues] = useState(Array(2).fill(0));
+  const [current, setCurrent] = useState(0);
+
+  // Utiliza o State Hook para atualizar o valor corrente cada vez que o
+  // display for atualizado
+  useEffect(() => {
+    // Cria um clone do array de valores utilizando o operador Spreed
+    const oldValues = [...values];
+    oldValues[current] = parseFloat(displayValue);
+    setValues(oldValues);
+  }, [displayValue]);
+
+  const addDigit = (digit) => {
+    // Verifia no valor corrente a existência do ponto
+    if (digit === '.' && values[current].toString().includes('.')) return;
+
+    // Define o novo valor corrente
+    let currentValue = displayValue === '0' || clearDisplay ? '' : displayValue;
+
+    // Inclui o valor 0 para quando a string for começar com .
+    if (digit === '.') currentValue = '0';
+
+    // atribui os valores calculados nas variáveis de estado
+    setDisplayValue(currentValue + digit);
+    setClearDisplay(false);
+  };
+
+  const clearMemory = () => {
+    // atribui os valores padrões nas variáveis de estado
+    setDisplayValue('0');
+    setClearDisplay(false);
+    setNextOperation(null);
+    setValues(Array(2).fill(0));
+    setCurrent(0);
+  };
+
+  const setOperation = (operation) => {
+    if (current === 0) {
+      setNextOperation(operation);
+      setCurrent(1);
+      setClearDisplay(true);
+    } else {
+      const equals = operation === '=';
+
+      // recupera os valores do array de forma desestruturada
+      let [firstValue, secondValue] = [...values];
+
+      try {
+        // Eval is Evil :)
+        // firstValue = eval(`${firstValue} ${nextOperation} ${secondValue}`);
+
+        if (nextOperation === '+') {
+          firstValue += secondValue;
+        } else if (nextOperation === '-') {
+          firstValue -= secondValue;
+        } else if (nextOperation === '*') {
+          firstValue *= secondValue;
+        } else if (nextOperation === '/') {
+          firstValue /= secondValue;
+        }
+      } catch (error) {
+        [firstValue] = values;
+      }
+
+      secondValue = 0;
+
+      // utilizada a função toString para remover o filler de 0 a direita
+      setDisplayValue(firstValue.toString());
+      setNextOperation(equals ? null : operation);
+      setCurrent(equals ? 0 : 1);
+      setClearDisplay(!equals);
+      setValues([firstValue, secondValue]);
+    }
+  };
+
   return (
     <Container>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <Engine>
-              <Footer>Engine: Hermes</Footer>
-            </Engine>
-          )}
-          <Body>
-            <SectionContainer>
-              <SectionTitle>Step One</SectionTitle>
-              <Text>
-                Edit <Highlight>App.js</Highlight> to change this screen and
-                then come back to see your edits.
-              </Text>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionTitle>See Your Changes</SectionTitle>
-              <SectionDescription>
-                <ReloadInstructions />
-              </SectionDescription>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionTitle>Debug</SectionTitle>
-              <SectionDescription>
-                <DebugInstructions />
-              </SectionDescription>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionTitle>Learn More</SectionTitle>
-              <SectionDescription>
-                Read the docs to discover what to do next:
-              </SectionDescription>
-            </SectionContainer>
-            <LearnMoreLinks />
-          </Body>
-        </ScrollView>
-      </SafeAreaView>
+      <Display value={displayValue} />
+      <Buttons>
+        <Button label="AC" triple onClick={clearMemory} />
+        <Button label="/" operation onClick={setOperation} />
+        <Button label="7" onClick={addDigit} />
+        <Button label="8" onClick={addDigit} />
+        <Button label="9" onClick={addDigit} />
+        <Button label="*" operation onClick={setOperation} />
+        <Button label="4" onClick={addDigit} />
+        <Button label="5" onClick={addDigit} />
+        <Button label="6" onClick={addDigit} />
+        <Button label="-" operation onClick={setOperation} />
+        <Button label="1" onClick={addDigit} />
+        <Button label="2" onClick={addDigit} />
+        <Button label="3" onClick={addDigit} />
+        <Button label="+" operation onClick={setOperation} />
+        <Button label="0" double onClick={addDigit} />
+        <Button label="." onClick={addDigit} />
+        <Button label="=" operation onClick={setOperation} />
+      </Buttons>
     </Container>
   );
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -1,48 +1,10 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
-
-export const ScrollView = styled.ScrollView`
-  background-color: #f3f3f3;
+export const Container = styled.View`
+  flex: 1;
 `;
 
-export const Engine = styled.View`
-  position: absolute;
-  right: 0;
-`;
-
-export const Body = styled.View`
-  background-color: #fff;
-`;
-
-export const SectionContainer = styled.View`
-  margin-top: 32px;
-  padding-right: 24px;
-  padding-left: 24px;
-`;
-
-export const SectionTitle = styled.Text`
-  font-size: 24px;
-  font-weight: 600;
-  color: #000;
-`;
-
-export const SectionDescription = styled.Text`
-  margin-top: 8px;
-  font-size: 18px;
-  font-weight: 400;
-  color: #444;
-`;
-
-export const Highlight = styled.Text`
-  font-weight: 700;
-`;
-
-export const Footer = styled.Text`
-  color: #444;
-  font-size: 12px;
-  font-weight: 600;
-  padding: 4;
-  padding-right: 12;
-  text-align: right;
+export const Buttons = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
