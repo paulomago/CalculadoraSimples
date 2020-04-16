eff7145b408e670217090f076d61cb97d443e57c
Paulo Eduardo Oliveira da Silva <pesilva23@outlook.com>
2020-04-16 13:18:36 -0300

    Crição do tema default (Light)

---
+++ src/styles/themes/light.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const Light = StyleSheet.create({
+  WhiteLabel: {
+    color: '#000',
+    backgroundColor: '#fff',
+    borderColor: '#888',
+  },
+  OrangeLabel: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+    borderColor: '#bbb',
+  },
+  Dysplay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  DisplayValue: {
+    color: '#fff',
+  },
+});
